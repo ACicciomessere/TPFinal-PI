2df599a64f615ff76a49a29a5a26240b56e1361f
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2022-12-07 18:39:00 -0300

    Update sensorsdataCDT.c
    Se pusieron comentarios para poder entender mejor el codigo

--- sensorsdataCDT.c
+++ sensorsdataCDT.c
@@ -1,20 +1,20 @@
 #include "sensorsdataCDT.h"
 
-typedef struct Q2{ 
+typedef struct Q2 { 
     int anio;
     unsigned int countWeek;
     unsigned int countWeekEnd;
-    struct Q2*tail;
-}Q2;
+    struct Q2 * tail;
+} Q2;
 
-typedef struct sensor{
-   char*name;
-   unsigned long int countTotal;
-   Q2*first;
-}sense;
+typedef struct sensor {
+   char * name;
+   unsigned long int countTotal; // Cantidad total de peatones registrados por el sensor
+   Q2 * first; // Puntero al primer elemento de una lista que contiene los datos divididos por anio 
+} sense;
 
-typedef struct sensorsdataCDT{ 
-    sense*vec;
-    size_t size;
-    unsigned int dim;
-}sensorsCDT;
+typedef struct sensorsdataCDT { 
+    sense * vec;
+    size_t size; // Cantidad de memoria reservada en el heap
+    unsigned int dim; // Cantidad de sensores
+} sensorsCDT;
