4d37cba0c3697d5723df93db2a18b859ef6c7561
AlekDG <66072172+AlekDG@users.noreply.github.com>
2022-12-07 18:28:48 -0300

    Cambio de CDT, me parece mas apropiado para los Queries

--- sensorsdataCDT.c
+++ sensorsdataCDT.c
@@ -1,26 +1,20 @@
 #include "sensorsdataCDT.h"
 
-typedef struct query1 {
-  char * name;
-  size_t count;
-  size_t dim;
-} query1;
+typedef struct Q2{ 
+    int anio;
+    unsigned int countWeek;
+    unsigned int countWeekEnd;
+    struct Q2*tail;
+}Q2;
 
-typedef struct query2 {
-  size_t countweek;
-  size_t countweekend;
-  int anio;
-  size_t dim;
-} query2;
+typedef struct sensor{
+   char*name;
+   unsigned long int countTotal;
+   Q2*first;
+}sense;
 
-typedef struct query3 {
-  int year;
-  size_t cantPeatones;
-  size_t dim;
-} query3;
-
-typedef struct sensorsdataCDT {
-  query1 * vecQ1;
-  query2 * vecQ2;
-  query3 * vecQ3;
-} sensorsdataCDT;
+typedef struct sensorsdataCDT{ 
+    sense*vec;
+    size_t size;
+    unsigned int dim;
+}sensorsCDT;
