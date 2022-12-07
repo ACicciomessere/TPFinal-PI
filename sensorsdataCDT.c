4b72c7d0f2ce6a4cb57e6011542b435f52d487ed
ACicciomessere <108630511+ACicciomessere@users.noreply.github.com>
2022-12-07 16:48:24 -0300

    Update sensorsdataCDT.c

--- sensorsdataCDT.c
+++ sensorsdataCDT.c
@@ -1,1 +1,27 @@
 #include "sensorsdataCDT.h"
+
+typedef struct query1{
+  char * name;
+  size_t count;
+}query1;
+
+typedef struct query2{
+  size_t countweek;
+  size_t countweekend;
+  int anio;
+}query2;
+
+typedef struct query3{
+  int anio;
+  size_t cantPeatones;
+}query3;
+
+typedef struct sensorsdataCDT{
+  query1 * vecQ1;
+  query2 * vecQ2;
+  query3 * vecQ3;
+}sensorsdataCDT;
+
+sensorsdataADT newSensorsDataADT(){
+  return calloc( 1, sizeof( sensorsdataCDT ) );
+}
