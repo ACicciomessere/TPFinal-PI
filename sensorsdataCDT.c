ce8cb60e25304970dc11eb80c86d173b6f872fdc
ACicciomessere <108630511+ACicciomessere@users.noreply.github.com>
2022-12-07 17:27:49 -0300

    Update sensorsdataCDT.c
    Cambio en la estructura

--- sensorsdataCDT.c
+++ sensorsdataCDT.c
@@ -1,27 +1,18 @@
 #include "sensorsdataCDT.h"
 
-typedef struct query1{
+typedef struct data{
+  unsigned int year;
+  unsigned int Mdate;
+  char * day;   // hacer un enum para guardarlo como numero
+  int hourly_counts;
+}data;
+
+typedef struct sensors{
   char * name;
-  size_t count;
-}query1;
-
-typedef struct query2{
-  size_t countweek;
-  size_t countweekend;
-  int anio;
-}query2;
-
-typedef struct query3{
-  int anio;
-  size_t cantPeatones;
-}query3;
+  char status;
+}sensors;
 
 typedef struct sensorsdataCDT{
-  query1 * vecQ1;
-  query2 * vecQ2;
-  query3 * vecQ3;
+  data * vecD;
+  sensors * vecS;
 }sensorsdataCDT;
-
-sensorsdataADT newSensorsDataADT(){
-  return calloc( 1, sizeof( sensorsdataCDT ) );
-}
