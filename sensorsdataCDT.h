200e3736af2ee746de46f907046b522c0734d909
AlekDG <66072172+AlekDG@users.noreply.github.com>
2022-12-07 16:57:28 -0300

    Para modificar el header desde github

---
+++ sensorsdataCDT.h
@@ -0,0 +1,8 @@
+#ifndef TPFINAL_PI_SENSORSDATACDT_H
+#define TPFINAL_PI_SENSORSDATACDT_H
+
+typedef struct sensorsdataCDT* sensorsdataADT;
+
+sensorsdataADT newSensorsDataADT(void);
+
+#endif //TPFINAL_PI_SENSORSDATACDT_H
