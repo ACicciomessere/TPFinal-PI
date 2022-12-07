808ffb7e806ced7cf37f5f693fedc5f065ef3c4e
ACicciomessere <108630511+ACicciomessere@users.noreply.github.com>
2022-12-07 18:56:16 -0300

    Update sensorsdataCDT.c

--- sensorsdataCDT.c
+++ sensorsdataCDT.c
@@ -17,4 +17,14 @@ typedef struct sensorsdataCDT{
     sense*vec;
     size_t size;
     unsigned int dim;
-}sensorsCDT;
+}sensorsdataCDT;
+
+sensorsdataADT newSensorsADT(){
+    return calloc( 1, sizeof( sensordataCDT ) );
+}
+
+void newSensor( sensordataADT sensor, int id, char * name, int anio, int Mdate, char * date, int hourly_counts ){
+    enum week{ Monday, Tuesday , Wednesday, Thursday, Friday, Saturday, Sunday };
+    enum week day;
+    day = date;    // hay que agarrar y agregar si el vector esta en NULL o si el año no esta en la lista. Si el año esta entonces hay que sumarle a donde corresponda
+}
