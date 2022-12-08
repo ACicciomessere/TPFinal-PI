5017a6d0152c7dba89a6125d976a97115ea990d4
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2022-12-08 17:43:28 -0300

    Update FrontEnd.c
    Primer prototipo del front end, tiene errores

--- FrontEnd.c
+++ FrontEnd.c
@@ -1,1 +1,30 @@
+int main() {
+      sensorsADT sensors = newSensorsADT();
 
+      int sensorSize = getSensorSize(sensors);
+      
+      //Query 1
+      orderByPeopleAmount(sensors);
+
+      for(int i = 0; i < sensorSize; i++) {
+            char * name = getSensorName(sensors, i);
+            if(name != NULL) {
+                  fprintf(...);
+            }
+      }
+
+      //Query 2
+      int year = getMaxYear(sensors);
+      toBegin(sensors);
+      //el print va a tener: el anio, va a tener dias de semana, dias de fin de semana, y total
+      for(int i = 0; !hasNext(sensors); next(sensors, i), i++) {
+            unsigned long int weekDaysCount = getWeekDaysCount(sensors, year);
+            unsigned long int weekendsDaysCount = getWeekendsDaysCount(sensors, year);
+            unsigned long int total = getTotalDaysCount(sensors, )          
+            fprintf(...);
+
+            year -= 1;
+      }
+
+      return 0;
+}
