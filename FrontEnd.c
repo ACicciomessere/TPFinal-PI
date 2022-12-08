b9a148e2a3cf141c69c3d20beb8750d41047dd36
AlekDG <66072172+AlekDG@users.noreply.github.com>
2022-12-08 17:50:13 -0300

    Update FrontEnd.c

--- FrontEnd.c
+++ FrontEnd.c
@@ -16,13 +16,15 @@ int main() {
       //Query 2
       int year = getMaxYear(sensors);
       toBegin(sensors);
+      unsigned long int weekDays=0;
+      unsigned long int weekEnds=0;
+      unsigned long int yearTotal=1;//Tentativo, va a estar la funcion adentro del fprintf como valor numerico
       //el print va a tener: el anio, va a tener dias de semana, dias de fin de semana, y total
-      for(int i = 0; !hasNext(sensors); next(sensors, i), i++) {
-            unsigned long int weekDaysCount = getWeekDaysCount(sensors, year);
-            unsigned long int weekendsDaysCount = getWeekendsDaysCount(sensors, year);
-            unsigned long int total = getTotalDaysCount(sensors, )          
+      for(int i = 0; !hasNext(sensors); next(sensors, i), i++) {//Planteo la condicion de salida que total=0;
+            yearTotal=getTotalCount(sensors,year,&weekEnds,&weekDays);
             fprintf(...);
-
+            weekEnds=0;
+            weekDays=0;
             year -= 1;
       }
 
